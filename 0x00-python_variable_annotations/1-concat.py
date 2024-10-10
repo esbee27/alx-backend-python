df96dd4aa6e337e20d1a61238132a15258d8f4e2
Blessing Ogor <ogorblessing96@gmail.com>
2024-10-10 15:52:45 +0000

    Tasks 0 to 3 done

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""Returns a concatenated string"""
+
+def concat(str1: str, str2: str) -> str:
+    """returns a string"""
+    return str1 + str2
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""Returns the floornof the float"""
+
+def floor(n: float) -> int:
+    """retuens the floor of the float"""
+    return int(n)
